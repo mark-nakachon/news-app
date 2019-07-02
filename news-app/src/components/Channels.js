2b79f340219bc10bf3bcecb9213256620f661a5c
harish chennupati <harishchennupati54@gmail.com>
2019-07-02 10:07:32 +0000

    Added filter channels based on category

--- news-app/src/components/Channels.js
+++ news-app/src/components/Channels.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Loading from './Loading';
+import Dropdown from './Dropdown';
+
+export function filterByCategory(query){
+    this.setState({category:query});
+    console.log(query);
+}
 class Channels extends Component {
   _isMounted = false;
   constructor(props) {
     super(props);
     this.state = {
-      channels: null
+      channels: null,
+      category:null,
+      filteredChannels:null
     };
+    filterByCategory = filterByCategory.bind(this);
   }
   componentDidMount() {
     this._isMounted = true;
@@ -20,16 +29,53 @@ class Channels extends Component {
           if(this._isMounted){this.setState({ channels: data.sources })}
         } );
   }
+  componentDidUpdate(prevProps,prevState){
+      if(prevState.category !== this.state.category){
+
+      const filteredData = this.state.channels.filter((obj)=>{
+          if (obj.category == this.state.category){
+              return true;
+          }
+      });
+      this.setState({filteredChannels:filteredData});
+      }
+  }
+
   componentWillUnmount(){
     this._isMounted = false;
   }
   render() {
-    const { channels } = this.state;
+    const { channels,filteredChannels } = this.state;
     console.log(channels);
-    if (channels != null) {
+    if(filteredChannels!==null){
+           return (
+        <div>
+          <h4 class="text-center text-light">New's Channels</h4>
+          <Dropdown />
+          <div class="container">
+          {filteredChannels.map(channel => (
+              <div class="card bg-light rounded p-1 mb-4">
+                <Link to={`/channel/${channel.id}`} style={{textDecoration:'None'}}>
+                <div class="card-header d-flex justify-content-between bg-dark">
+                  <h5 class="text-light">{channel.name}</h5>
+                  <h5 class="text-danger"><span class="text-info mr-2">Category:</span>{channel.category.charAt(0).toUpperCase()+channel.category.slice(1)}</h5>
+                  </div>
+                <div class="card-body">
+                  <h5 class="card-title text-dark">{channel.description}</h5>
+
+                </div>
+                </Link>
+              </div>
+          ))}
+          </div>
+        </div>
+      );
+    }
+    else if (channels !== null) {
       return (
         <div>
           <h4 class="text-center text-light">New's Channels</h4>
+          <Dropdown />
           <div class="container">
           {channels.map(channel => (
               <div class="card bg-light rounded p-1 mb-4">
---
+++ news-app/src/components/Dropdown.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {filterByCategory} from './Channels';
+export default class Dropdown extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.category = this.category.bind(this);
+    }
+    category(e){
+        e.preventDefault();
+        console.log(e.target.value);
+        filterByCategory(e.target.value);
+    }
+    render() {
+     return(
+         <select class="browser-default custom-select custom-select-lg mb-3" onChange={this.category}>
+            <option selected>Filter by category</option>
+            <option value="business">Business</option>
+            <option value="entertainment">Entertainment</option>
+            <option value="general">General</option>
+            <option value="health">Health</option>
+            <option value="science">Science</option>
+            <option value="sports">Sports</option>
+            <option value="technology">Technology</option>
+        </select>
+     );
+    }
+ }
