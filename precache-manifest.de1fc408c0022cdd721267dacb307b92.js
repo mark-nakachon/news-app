8523ee2ce3feb3d9864a58c9ca7ec70d5af3b38f
mark-nakachon <harishchennupati54@gmail.com>
2019-07-02 23:35:33 +0530

    Updates

---
+++ precache-manifest.de1fc408c0022cdd721267dacb307b92.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f336402294ab298bf6874c3a232c244",
+    "url": "/news-app/index.html"
+  },
+  {
+    "revision": "662364de344930cb70cf",
+    "url": "/news-app/static/css/2.d9e5d4ab.chunk.css"
+  },
+  {
+    "revision": "6ef29a293b661557b4e0",
+    "url": "/news-app/static/css/main.4e78c92a.chunk.css"
+  },
+  {
+    "revision": "662364de344930cb70cf",
+    "url": "/news-app/static/js/2.24e39910.chunk.js"
+  },
+  {
+    "revision": "6ef29a293b661557b4e0",
+    "url": "/news-app/static/js/main.6c778d87.chunk.js"
+  },
+  {
+    "revision": "fa4b01bc436166f464fa",
+    "url": "/news-app/static/js/runtime~main.dc36b5e3.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/news-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/news-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/news-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/news-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/news-app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
