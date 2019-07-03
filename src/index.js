b349ebc319ca150cdd4b029c43f6a6df026c965a
mark-nakachon <harishchennupati54@gmail.com>
2019-07-03 08:55:02 +0530

    finished assignment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap.css';
+import 'bootswatch/dist/solar/bootstrap.css';
 import 'font-awesome/css/font-awesome.min.css'
 import {BrowserRouter as Router} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
